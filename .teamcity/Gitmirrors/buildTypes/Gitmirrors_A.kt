cd74b45714168a55a8f2c0317ae7a6958f986913
admin <admin@TeamCity>
2022-02-14 14:59:10 +0100

    TeamCity change in 'gitmirrors' project: Cleanup old configuration files

--- .teamcity/Gitmirrors/buildTypes/Gitmirrors_A.kt
+++
@@ -1,8 +0,0 @@
-package Gitmirrors.buildTypes
-
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-
-object Gitmirrors_A : BuildType({
-    uuid = "6deaa30b-6862-44c4-8bc3-d4de28dbd57c"
-    name = "a"
-})
--- .teamcity/Gitmirrors/buildTypes/Gitmirrors_B.kt
+++
@@ -1,8 +0,0 @@
-package Gitmirrors.buildTypes
-
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-
-object Gitmirrors_B : BuildType({
-    uuid = "275f72b8-ccc9-494e-8f90-1c8f28f9d4de"
-    name = "b2"
-})
