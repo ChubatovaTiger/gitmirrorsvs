192abf592361a25cb31b03123898cedec141474b
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-02-14 15:32:27 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -43,7 +43,7 @@ object A : BuildType({
             scriptContent = "echo acd"
         }
         script {
-            scriptContent = "sss"
+            scriptContent = "sssa"
         }
     }
 })
