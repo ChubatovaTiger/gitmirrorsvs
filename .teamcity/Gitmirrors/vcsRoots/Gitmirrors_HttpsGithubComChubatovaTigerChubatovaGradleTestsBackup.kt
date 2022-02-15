c69134445d27274bcf5aece5bb65960ba2618c69
admin <admin@TeamCity>
2022-02-15 14:11:22 +0100

    TeamCity change in 'gitmirrors' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/Gitmirrors/vcsRoots/Gitmirrors_HttpsGithubComChubatovaTigerChubatovaGradleTestsBackup.kt
@@ -0,0 +1,11 @@
+package Gitmirrors.vcsRoots
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+
+object Gitmirrors_HttpsGithubComChubatovaTigerChubatovaGradleTestsBackup : GitVcsRoot({
+    uuid = "315a50a4-9ae6-499e-979b-080a1f8e05b3"
+    name = "https://github.com/ChubatovaTiger/ChubatovaGradleTestsBackup"
+    url = "https://github.com/ChubatovaTiger/ChubatovaGradleTestsBackup"
+    useMirrors = false
+})
